4f0b9bf8ca10ae32829dc22a6cac61d7b2a4398b
Christian Hey <c.hey@sbs-herzogenaurach.de>
2025-04-09 09:12:09 +0200

    Added mousePressEvent to switch between back and front cover

--- MemoryCard.py
+++ MemoryCard.py
@@ -1,3 +1,4 @@
+from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QPixmap
 from PyQt6.QtWidgets import QLabel
 
@@ -6,6 +7,21 @@ class MemoryCard(QLabel):
     def __init__(self, file_name, parent=None):
         super(MemoryCard, self).__init__(parent)
 
-        pixmap = QPixmap(file_name).scaledToWidth(400)
+        self.__shows_back = True
 
-        self.setPixmap(pixmap)
+        self.__ident = file_name
+
+        self.__pixmap_back = QPixmap("card_back.png").scaledToWidth(400, Qt.TransformationMode.SmoothTransformation)
+        self.__pixmap_front = QPixmap(file_name).scaledToWidth(400, Qt.TransformationMode.SmoothTransformation)
+
+        self.setPixmap(self.__pixmap_back)
+
+    def mousePressEvent(self, event):
+        if self.__shows_back:
+            self.__shows_back = False
+
+            self.setPixmap(self.__pixmap_front)
+        else:
+            self.__shows_back = True
+
+            self.setPixmap(self.__pixmap_back)
