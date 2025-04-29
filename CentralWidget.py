9d7af888ad3cc4f353980ba57fdabe37bb9b7e8c
Christian Hey <c.hey@sbs-herzogenaurach.de>
2025-04-29 13:44:43 +0200

    Added logic to find pairs

--- CentralWidget.py
+++ CentralWidget.py
@@ -1,3 +1,4 @@
+from PyQt6.QtCore import pyqtSlot, QTimer
 from PyQt6.QtWidgets import QWidget, QGridLayout
 
 from MemoryCard import MemoryCard
@@ -8,21 +9,51 @@ import random as rnd
 class CentralWidget(QWidget):
     def __init__(self, parent=None):
         super(CentralWidget, self).__init__(parent)
+        self.timer = QTimer()
+
+        self.first_card = None
+        self.second_card = None
 
         layout = QGridLayout(parent)
 
         memory_cards = [MemoryCard("cat_1.png"), MemoryCard("cat_1.png"), MemoryCard("cat_2.png"),
-                        MemoryCard("cat_2.png")]
-        # memory_cards.append(MemoryCard("cat_3.png"))
-        # memory_cards.append(MemoryCard("cat_3.png"))
-        # memory_cards.append(MemoryCard("cat_4.png"))
-        # memory_cards.append(MemoryCard("cat_4.png"))
+                        MemoryCard("cat_2.png"), MemoryCard("cat_3.png"), MemoryCard("cat_3.png"),
+                        MemoryCard("cat_4.png"), MemoryCard("cat_4.png")]
 
         rnd.shuffle(memory_cards)
 
         counter = 0
         for card in memory_cards:
+            card.card_pressed.connect(self.card_listener)
+
             layout.addWidget(card, counter // 4, counter % 4)
             counter += 1
 
         self.setLayout(layout)
+
+    @pyqtSlot()
+    def card_listener(self):
+        if self.first_card is None:
+            self.first_card = self.sender()
+            self.first_card.setDisabled(True)
+        else:
+            self.second_card = self.sender()
+
+            if self.second_card.ident == self.first_card.ident:
+                self.second_card.setDisabled(True)
+                print("Paar gefunden")
+            else:
+                self.first_card.setDisabled(False)
+
+                self.setDisabled(True)
+                self.timer.singleShot(5 * 1000, self.flip_cards)
+
+    @pyqtSlot()
+    def flip_cards(self):
+        self.first_card.flip_card()
+        self.first_card = None
+
+        self.second_card.flip_card()
+        self.second_card = None
+
+        self.setDisabled(False)
--- MemoryCard.py
+++ MemoryCard.py
@@ -1,15 +1,17 @@
-from PyQt6.QtCore import Qt
+from PyQt6.QtCore import Qt, pyqtSignal
 from PyQt6.QtGui import QPixmap
 from PyQt6.QtWidgets import QLabel
 
 
 class MemoryCard(QLabel):
+    card_pressed = pyqtSignal()
+
     def __init__(self, file_name, parent=None):
         super(MemoryCard, self).__init__(parent)
 
         self.__shows_back = True
 
-        self.__ident = file_name
+        self.ident = file_name
 
         self.__pixmap_back = QPixmap("card_back.png").scaledToWidth(400, Qt.TransformationMode.SmoothTransformation)
         self.__pixmap_front = QPixmap(file_name).scaledToWidth(400, Qt.TransformationMode.SmoothTransformation)
@@ -17,6 +19,11 @@ class MemoryCard(QLabel):
         self.setPixmap(self.__pixmap_back)
 
     def mousePressEvent(self, event):
+        self.flip_card()
+
+        self.card_pressed.emit()
+
+    def flip_card(self):
         if self.__shows_back:
             self.__shows_back = False
 
